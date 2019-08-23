df4b0264971b45a109d989809fdcc528b9d72f13
JacFox11 <49622962+JacFox11@users.noreply.github.com>
2019-08-23 13:20:14 -0600

    update

---
+++ Liga.cpp
@@ -0,0 +1,59 @@
+#include<string.h>
+#include <vector>
+#include <bits/stdc++.h>
+#include<iostream> 
+#include<string> 
+#include <fstream>
+#include <stdlib.h>
+
+using namespace std;
+
+#ifndef Liga_CPP
+#define Liga_CPP
+
+class Liga{
+	private:
+		string nombre;
+		string pais;
+		vector <Equipo*> equipos;
+	public:
+		Liga(){
+		}
+		
+		Liga(string nombre, string pais){
+			this->nombre=nombre;
+			this->pais=pais;
+		}
+		
+		string getNombre(){
+			return nombre;
+		}
+		
+		void setNombre(string nombre){
+			this->nombre=nombre;
+		}
+		
+		string getPais(){
+			return pais;
+		}
+		
+		void setPais(string pais){
+			this->pais=pais;
+		}
+		
+		vector <Equipo*> getEquipo(){
+			return equipos;
+		}
+		
+		void setEquipo(Equipo* equipo){
+			equipos.push_back(equipo); 
+		}
+		
+		void remEquipo(int pos){
+			equipos.erase(equipos.begin() + pos); 
+		}
+		
+		~Liga(){}
+};
+
+#endif
