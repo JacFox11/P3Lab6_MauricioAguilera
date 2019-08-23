1427f3bd0c5fb5f07fd4d6374ac2d0e4aaa63ec6
JacFox11 <49622962+JacFox11@users.noreply.github.com>
2019-08-23 13:31:29 -0600

    update

---
+++ Jugador.cpp
@@ -0,0 +1,56 @@
+#include <string.h>
+#include <vector>
+#include <bits/stdc++.h>
+#include<iostream> 
+#include<string> 
+#include <fstream>
+#include <stdlib.h>
+
+using namespace std;
+
+#ifndef Jugador_CPP
+#define Jugador_CPP
+
+class Jugador{
+	private:
+		string nombre;
+		int dorsal;
+		string pais;
+	public:
+		Jugador(){
+		}
+		
+		Jugador(string nombre, int dorsal, string pais){
+			this->nombre=nombre;
+			this->dorsal=dorsal;
+			this->pais=pais;
+		}
+		
+		string getNombre(){
+			return nombre;
+		}
+		
+		void setNombre(string nombre){
+			this->nombre=nombre;
+		}
+		
+		int getDorsal(){
+			return dorsal;
+		}
+		
+		void setDorsal(int dorsal){
+			this->dorsal=dorsal;
+		}
+		
+		string getPais(){
+			return pais;
+		}
+		
+		void setPais(string pais){
+			this->pais=pais;
+		}
+		
+		~Jugador(){}
+};
+
+#endif
